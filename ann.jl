588f266471aa5e2dc7b651d44d58ee4913f57c51
americast <sayan.sinha@iitkgp.ac.in>
2017-03-29 07:38:41 +0530

    SGD added

--- ann.jl
+++ ann.jl
@@ -17,11 +17,27 @@ if length(hidden)>0
   push!(theta,rand(hidden[end],1))  #change this to length of each output
 end
 
+
 print("How many iterations? ")
 n=parse(Int,readline(STDIN))
-Delta
+print("Rate?: ")
+eta=parse(Int,readline(STDIN))
 for i in (1:n)
   for j in (1:length(input[1,:]))
-    a=compute(input[:,j],hidden,theta)
-    delta=backprop(a,output[j],theta)
+    Delta=[]
+    for i in (1:length(hidden)+1)
+      push!(Delta,0)
+    end
+    send=input[:j]
+    m=length(send)
+    for k in send
+      a=compute(k,hidden,theta)
+      delta=backprop(a,output[j],theta)
+      for l in (1:length(delta))
+        Delta[l]+=delta[l]
+      end
+    end
+    for l in (1:length(Delta))
+      theta[l]-=(eta/m)*Delta #Add regularisation
+  end
 end
--- learn.jl
+++ learn.jl
@@ -1,6 +1,14 @@
+function sigmoid(result)
+  for i in (1:length(result))
+    result[i]=(1/(1+(e**result[i])))
+  end
+  return result
+end
+
 function compute(input,hidden,theta)
   a=[]
-  resulthere=input' * theta[1]
+  resulthere=input * theta[1]
+  resulthere=sigmoid(resulthere)
   push!(a,resulthere)
   for j in (2:length(theta))
     resulthere*=theta[i]
@@ -12,8 +20,8 @@ end
 function backprop(a, output,theta)
   delta=[]; j=length(theta)
   push!(delta,a[end]-output)
-  for i in (length(a)-1:-1:1)
-    deltahere=(theta[j])'*delta[1] .* a[i] (.* (1-a[i]))
+  for i in (length(a)-1:-1:2)
+    deltahere=(theta[j])'*delta[1] .* (a[i] .* (1-a[i]))
     unshift!(delta,deltahere)
     j-=1
   end
